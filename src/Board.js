2be47a5910321d0466f6380acf474a192c2d269d
kyle <kyle.dick0811@gmail.com>
2022-06-27 22:07:43 -0600

    "Complete board through col conflicts"

--- src/Board.js
+++ src/Board.js
@@ -79,41 +79,130 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
+      //check if row inde has conflicts
+
+      //create a count var
+      var count = 0;
+      //use this.get to the row we want to check with the row index
+      var index = this.get(rowIndex);
+      //for each item in the row
+      _.each(index, function(element) {
+        if (element === 1) {
+          count++;
+        }
+      });
+      if (count > 1) {
+        return true;
+      }
+      //check count var if its larger than one than return true otherwise return false
       return false; // fixme
     },
-
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      //reuse hasRowConflictAt function for each row
+      var rows = this.rows();
+      //loop over thr rows and check each rowIndex
+      for (var i = 0; i < rows.length; i++) {
+        //reusing hasRowConflictAt function for each rowIndex
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+        //if it once returned true we return true;
+      }
       return false; // fixme
     },
-
-
 
     // COLUMNS - run from top to bottom
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      /*
+      [[1,0,0],
+       [1,0,0],
+       [0,1,0]]
+       colindex = 0; true
+       colIndex = 1; false;
+      */
+      var count = 0;
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.get(i)[colIndex] === 1) {
+          count ++;
+        }
+      }
+      if (count > 1) {
+        return true;
+      }
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      // reuse hasColConflictsAt for each colums
+      var a = this.get('n');
+      // a = 4;
+      for (var i = 0; i < a; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
-
-
 
     // Major Diagonals - go from top-left to bottom-right
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+      var n = this.get('n');
+      var count = 0;
+      if (majorDiagonalColumnIndexAtFirstRow === n - 1) {
+        return false;
+      }
+      var find = false;
+      var rowIndex;
+      //loop all the row with the colIndex
+      for (var i = 0; i < n; i++) {
+        var cur = this.get(i)[majorDiagonalColumnIndexAtFirstRow];
+        if (cur === 1) {
+          rowIndex = i;
+          find = true;
+          break;
+        }
+      }
+      /*
+      [[1,0,1],
+       [0,0,1],
+       [1,1,1]]
+       rowIndex = 0;colIndex = 0;
+       nextRow =rowIndex + 1 = 1
+       nextCol = colIndex + 1;
+      */
+      if (!find) {
+        return false;
+      }
+      if (i === n - 1) {
+        return false;
+      }
+      //for loop rest row to find the diagonal element;
+      var nextColIndex = majorDiagonalColumnIndexAtFirstRow + 1;
+      for (var j = rowIndex + 1; j < n; j++) {
+        if (nextColIndex < n && this.get(j)[nextColIndex] === 1) {
+          count ++;
+        } else {
+          nextColIndex ++;
+        }
+      }
+      if (count > 0) {
+        return true;
+      }
+      // if the diagonal element === 1 = > count ++;
       return false; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      //pass each columIndex to test if it return conflict
       return false; // fixme
     },
 
